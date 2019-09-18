3ec5e947d65c507e6cea788b9df8927aaf9f2bff
M. S <10941563+madblade@users.noreply.github.com>
2019-09-18 15:34:22 +0200

    fix: build

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports =
 
     output: {
         path: path.resolve(__dirname, './dist'),
-        publicPath: '../models/',
+        publicPath: './',
         filename: '[name].[hash].js'
     },
 
